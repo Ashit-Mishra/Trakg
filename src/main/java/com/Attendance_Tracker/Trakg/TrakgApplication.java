de6c658d50f3047894f875bb6303e772eea04d4f
Ashit Mishra <asitm275@gmail.com>
2026-07-30 23:54:26 +0530

    Initialised project

---
+++ src/main/java/com/Attendance_Tracker/Trakg/TrakgApplication.java
@@ -0,0 +1,13 @@
+package com.Attendance_Tracker.Trakg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrakgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrakgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Attendance_Tracker/Trakg/TrakgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Attendance_Tracker.Trakg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrakgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
